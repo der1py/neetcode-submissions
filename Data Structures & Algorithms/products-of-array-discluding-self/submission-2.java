a90da4271547812c213e1a828787c172d945c32a
deripy <151796113+der1py@users.noreply.github.com>
2026-05-23 10:17:43 -0700

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] prefix = new int[nums.length];
+        int[] suffix = new int[nums.length];
+        int[] output = new int[nums.length];
+
+        int prod = 1;
+        for (int i = 0; i < nums.length; i++) {
+            prefix[i] = prod;
+            prod *= nums[i];
+        }
+
+        prod = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            suffix[i] = prod;
+            prod *= nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            output[i] = prefix[i] * suffix[i];
+        }
+
+        return output;
+    }
+}  
